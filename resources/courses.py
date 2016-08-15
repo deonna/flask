23abeaf2ede54ed06641e19c63965696de2fa189
deonna <dnhodges@gmail.com>
2016-08-14 22:58:38 -0700

    feat(course-api): Adding validation to single course and ensuring GET returns actual data from DB instead of mocked data.

--- resources/courses.py
+++ resources/courses.py
@@ -1,6 +1,9 @@
 from flask import jsonify, Blueprint
-from flask.ext.restful import (Resource, Api, reqparse, 
-            inputs, fields, marshal, marshal_with, url_for)
+from flask.ext.restful import (
+            Resource, Api, reqparse, 
+            inputs, fields, marshal, 
+            marshal_with, url_for, abort
+            )
 
 import models
 
@@ -15,6 +18,14 @@ def add_reviews(course):
     course.reviews = [url_for('resources.reviews.review', id=review.id)
                       for review in course.review_set]
     return course                    
+
+def course_or_404(course_id):
+    try:
+        course = models.Course.get(models.Course.id==course_id)
+    except models.Course.DoesNotExist:
+        abort(404, message="Course {} does not exist.".format(course_id))
+    else:
+        return course
 
 class CourseList(Resource):
 
@@ -47,8 +58,25 @@ class CourseList(Resource):
         return jsonify({'courses': {'title': 'Python Basics'}})
 
 class Course(Resource):
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument(
+            'title',
+            required=True,
+            help='No course title provided',
+            location=['form', 'json']
+        )
+        self.reqparse.add_argument(
+            'url',
+            required=True,
+            help='No course URL provided',
+            location=['form', 'json'],
+            type=inputs.url
+        )
+
+    @marshal_with(course_fields)        
     def get(self, id):
-        return jsonify({'title': 'Python Basics'})        
+        return add_reviews(course_or_404(id))      
 
     def put(self, id):
         return jsonify({'title': 'Python Basics'})        
